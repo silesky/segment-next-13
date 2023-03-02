4f3a1d9756cd168b80cfaa27213c50c269f56c5c
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-03-01 22:53:10 -0600

    update app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { AnalyticsBrowser, Plugin } from "@segment/analytics-next";
+import { AnalyticsBrowser } from "@segment/analytics-next";
 
-const myPlugin: Plugin = {
-  name: "My Plugin",
-  type: "enrichment",
-  version: "0.0.1",
-  load: () => Promise.resolve(),
-  isLoaded: () => true,
-  track: (ctx) => {
-    ctx.updateEvent("properties.kiwi", "🥝"); // add property here
-    return ctx;
-  },
-};
-
-export const analytics = new AnalyticsBrowser();
-analytics.load({ writeKey: "9lSrez3BlfLAJ7NOChrqWtILiATiycoc" });
-
+export const analytics = AnalyticsBrowser.load({ writeKey: "9lSrez3BlfLAJ7NOChrqWtILiATiycoc" })
 analytics.page()
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
